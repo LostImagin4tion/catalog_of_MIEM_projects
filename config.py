75be801e9032fea238ee4d8c541dd73b99b623d2
egor danilov <evdanilov_2@miem.hse.ru>
2022-04-11 16:34:16 +0300

    feat: added all functions for getting info about projects

---
+++ config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+from typing import Optional
+
+
+class Settings(BaseSettings):
+    """ Instance stores all app settings, mainly environment variables """
+
+    BASE_URL: Optional[str]
+    PUBLIC_API: Optional[str]
+
+    class Config:
+        env_file = r'C:\Users\user\PycharmProjects\pythonDed\.env'
+        env_file_encoding = 'utf-8'
+
+        #uncomment when testing
+        #env_prefix = 'TEST_' + env_prefix
+
+
+settings = Settings()
---
+++ get.py
@@ -0,0 +1,71 @@
+import requests
+import json
+from pydantic import Required
+
+from config import settings
+
+
+def all_projects() -> json:
+    response = requests.get(
+        f'{settings.BASE_URL}/{settings.BASE_URL}/projects'
+    ).json()
+
+    return response
+
+
+def project_link(project_id: Required[int]) -> str:
+    return f'https://cabinet.miem.hse.ru/#/project/{project_id}'
+
+
+def project_details(project_id: Required[int]) -> json:
+    response = requests.get(
+        f'{settings.BASE_URL}/{settings.BASE_URL}/project/body/{project_id}'
+    ).json()
+
+    return response
+
+
+def project_vacancies(project_id: Required[int]) -> json:
+    response = requests.get(
+        f'{settings.BASE_URL}/{settings.BASE_URL}/project/vacancies/{project_id}'
+    )
+
+    return response
+
+
+def project_team(project_id: Required[int]) -> json:
+    response = requests.get(
+        f'{settings.BASE_URL}/{settings.BASE_URL}/project/{project_id}/team'
+    )
+
+    return response
+
+
+def userstories_stats() -> json:
+
+    headers = {
+        'content-type': 'application/json',
+        'x-disable-pagination': 'true'
+    }
+
+    taiga_response = requests.get(
+        'https://track.miem.hse.ru/api/v1/userstories?',
+        headers=headers
+    ).json()
+
+    return taiga_response
+
+
+def get_tasks_stats() -> json:
+
+    headers = {
+        'content-type': 'application/json',
+        'x-disable-pagination': 'true'
+    }
+
+    taiga_response = requests.get(
+        'https://track.miem.hse.ru/api/v1/tasks',
+        headers=headers
+    ).json()
+
+    return taiga_response
